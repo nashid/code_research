ff5dc6c6b0600bb574229cb231ea23715f244a26
nancy <npsecoding@gmail.com>
2018-02-10 15:59:21 -0600

    update model script

--- Model/train.sh
+++ Model/train.sh
@@ -3,10 +3,10 @@
 python -m nmt.nmt \
     --attention=scaled_luong \
     --src=buggy --tgt=correct \
-    --vocab_prefix=./data/vocab  \
-    --train_prefix=./data/train \
-    --dev_prefix=./data/dev  \
-    --test_prefix=./data/test \
+    --vocab_prefix=./data/Vocab/vocab  \
+    --train_prefix=./data/TrainData/train \
+    --dev_prefix=./data/DevData/dev  \
+    --test_prefix=./data/TestData/test \
     --out_dir=./data/nmt_model \
     --num_train_steps=1000 \
     --steps_per_stats=100 \
