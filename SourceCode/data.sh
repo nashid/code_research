bb186d3ee5c416d298e08d10aa959c505865701e
nancy <npsecoding@gmail.com>
2018-02-10 14:13:03 -0600

    update data script

--- SourceCode/data.sh
+++ SourceCode/data.sh
@@ -9,16 +9,12 @@ cp -r './TrainingData/' '../Model/data/'
 
 n=100 npm run mutate
 npm run tokenize
-npm run generate_vocab
-python commonvocab.py 20
 fn='./TestData/' npm run training_data
 cp -r './TestData/' '../Model/data/'
 
 n=100 npm run mutate
 npm run tokenize
-npm run generate_vocab
-python commonvocab.py 20
 fn='./DevData/' npm run training_data
 cp -r './DevData/' '../Model/data/'
 
-
+cp -r './Vocab' '../Model/data/'
