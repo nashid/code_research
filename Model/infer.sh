c158da5412b78d718eec33a94707fef7d0a1b8d2
nancy <npsecoding@gmail.com>
2018-03-13 17:01:57 -0500

    generated data

--- Model/infer.sh
+++ Model/infer.sh
@@ -3,6 +3,6 @@
 rm 'data/code_infer.correct'
 
 python -m nmt.nmt \
-    --out_dir=./data/nmt_model \
+    --out_dir=./code_model \
     --inference_input_file=./data/code_infer.buggy \
     --inference_output_file=./data/code_infer.correct
--- Model/train.sh
+++ Model/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf './data/nmt_model'
+rm -rf './code_model'
 
 python -m nmt.nmt \
     --src=buggy --tgt=correct \
@@ -8,14 +8,14 @@ python -m nmt.nmt \
     --train_prefix=./data/TrainData/train \
     --dev_prefix=./data/DevData/dev  \
     --test_prefix=./data/TestData/test \
-    --out_dir=./data/nmt_model \
-    --encoder_type=bi \
-    --optimizer=adam \
-    --learning_rate=0.1 \
-    --batch_size=64 \
+    --out_dir=./nmt_model \
+    #--encoder_type=bi \
+    --optimizer=sgd \
+    --learning_rate=0.3 \
+    #--batch_size=1024 \
     --num_train_steps=12000 \
     --steps_per_stats=100 \
-    --num_layers=2 \
-    --num_units=256 \
-    --dropout=0.3 \
+    --num_layers=1 \
+    --num_units=1500 \
+    --dropout=0.2 \
     --metrics=bleu
