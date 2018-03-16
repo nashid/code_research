0c457e5bf02f188921618c20fa9c91d98d3fe99a
nancy <npsecoding@gmail.com>
2018-03-16 10:29:43 -0500

    Initial Model and scripts

--- Model/data/displayline.sh
+++ Model/data/displayline.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# sh displayline.sh code_infer.buggy code_actual.correct code_infer.correct <LN>
 
 FILE1=$1
 FILE2=$2
-LN="$3p"
+FILE3=$3
+LN="$4p"
 
 echo $FILE1
 echo $(sed -n $LN $FILE1)
 
 echo $FILE2
 echo $(sed -n $LN $FILE2)
+
+echo $FILE3
+echo $(sed -n $LN $FILE3)
